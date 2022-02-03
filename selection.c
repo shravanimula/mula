f223021bb6e47fcd1c3a9b803fe48f313e810025
Shravani <shravani.mula@thundersoft.com>
2022-02-03 19:50:03 +0530

    this is selection

---
+++ selection.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void selectionSort(int arr[], int size);
+void swap(int *a, int *b);
+void selectionSort(int arr[], int size)
+{
+	int i, j;
+	for (i = 0 ;  i < size;i++)
+	{
+       		for (j = i ; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+				swap(&arr[i], &arr[j]);
+		}
+	}
+}
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+     	*a = *b;
+	*b = temp;
+}
+int main()
+{
+	int array[10], i, size;
+	printf("number of elemtes:  ");
+	scanf("%d", &size);
+	printf("\nEnter %d numbers\t", size);
+	printf("\n");
+	for (i = 0; i < size; i++)
+		scanf("%d", &array[i]);
+	selectionSort(array, size);
+	printf("\nSorted array is ");
+	for (i = 0; i < size;i++)
+		printf(" %d ", array[i]);
+	return 0;
+}
+//best case=>O(n^2)
+//worst case=>O(n^2)
