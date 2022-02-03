83e171efcaa91f470143125c9ce108dab67972f1
Shravani <shravani.mula@thundersoft.com>
2022-02-03 19:29:12 +0530

    thi is bubblesort

---
+++ bubble.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+void bubblesort(int arr[], int size)
+{
+	int i, j;
+	for (i = 0;  i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (arr[j] > arr[j+1])
+				swap(&arr[j], &arr[j+1]);
+
+		}
+	}
+}
+void swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+int main()
+{
+    	int array[30], i, size;
+	printf("number of elements");
+	scanf("%d", &size);
+	printf("\nEnter %d numbers : ", size);
+	for (i = 0; i < size; i++)
+		scanf("%d", &array[i]);
+	bubblesort(array, size);
+	printf("\nSorted array is ");
+
+	for (i = 0; i < size; i++)
+		printf(" %d ", array[i]);
+	return 0;
+}	
+//best case=>O(n)
+//worst case=>o(n^2)
+//space complexity=>O(1)
